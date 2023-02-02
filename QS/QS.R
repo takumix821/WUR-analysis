dcb14c22dd8fbd34500105a57e81714cedcb6d26
takumix821 <alionking821@gmail.com>
2023-02-02 23:20:44 +0800

    renew 2023

--- QS/QS.R
+++ QS/QS.R
@@ -1,3 +1,5 @@
+# java -Dwebdriver.chrome.driver=D:chromedriver.exe -jar selenium-server-standalone-4.0.0-alpha-1.jar
+
 library("RSelenium")
 library(stringr)
 
@@ -8,26 +10,42 @@ remDr <- remoteDriver(
 
 remDr$open()
 
-remDr$navigate("https://www.topuniversities.com/university-rankings/world-university-rankings/2022")
+remDr$navigate("https://www.topuniversities.com/university-rankings/world-university-rankings/2023")
 
 Sys.sleep(5)
 
 # href table
 all_href_list = c()
+all_univ_name = c()
+all_univ_rank = c()
 all_univ_n = 0
 
-for (t in 1:130){
+
+page.list = remDr$findElements(using = 'class name', value = "page-link")
+maxpage = as.numeric(page.list[[length(page.list)-1]]$getElementText())
+
+for (t in 1:(maxpage-5)){
   univ.list = remDr$findElements(using = 'class name', value = "uni-link")
+  univ.rank = remDr$findElements(using = 'css selector', value = "div._univ-rank.hide-this-in-mobile-indi")
   univ_n = length(univ.list)
   all_univ_n = all_univ_n + univ_n
   href_list = c()
+  name_list = c()
+  rank_list = c()
   
   for (i in 1:univ_n){
     univ.elem = univ.list[[i]]
+    rank.elem = univ.rank[[i]]
     univ_href = univ.elem$getElementAttribute("href")
+    univ_name = univ.elem$getElementText()
+    univ_rank = rank.elem$getElementText()
     href_list[i] = univ_href[[1]]
+    name_list[i] = univ_name[[1]]
+    rank_list[i] = univ_rank[[1]]
   }
   all_href_list = c(all_href_list, href_list)
+  all_univ_name = c(all_univ_name, name_list)
+  all_univ_rank = c(all_univ_rank, rank_list)
   
   n_page = remDr$findElements(using = 'class name', value = "page-link")
   n_page = n_page[[length(n_page)]]
@@ -36,13 +54,12 @@ for (t in 1:130){
 }
 
 # ranking table
-qs_ranking.table = data.frame(matrix(nrow = 0, ncol = 14))
+qs_ranking.table = data.frame(matrix(nrow = 0, ncol = 10))
 value = c("University", "Total students", "PG students",
           "UG students", "International students",
           "PG students", "UG students",
           "Total faculty staff", "Int'l staff",
-          "Domestic staff", "Status",
-          "Research Output", "Size", "rank")
+          "Domestic staff")
 colnames(qs_ranking.table) = value
 
 
@@ -50,56 +67,39 @@ for(i in 1:all_univ_n){
   remDr$navigate(all_href_list[i])
   Sys.sleep(5)
   
-  try({
-  
   # staff 子畫面
-  target = remDr$findElement(using = 'id', value = "staff")
+  target = remDr$findElement(using = 'id', value = "studStaff_Tab")
   target$sendKeysToElement(list("laptops", key="enter"))
   
-  # student 人數
-  student.elem = remDr$findElement(using = 'class name', value = "student")
-  student.elem = student.elem$findElement(using = 'class name', value = "line")
-  student = student.elem$getElementText()
+  # subsection (stud, inter, staff)
   
-  str_student = str_split(student, "\n")
+  studstaff = remDr$findElements(using = 'class name', value = 'studstaff-subsection')
+  studstaff.list = c()
+  for (i in 1:length(studstaff)){
+    sub.elem.list = studstaff[[i]]$findElements(using = 'class name', value = 'color-code-cont')
+    sub.list.char = unlist(lapply(sub.elem.list, function(e) { e$getElementText() }))[1]
+    sub.list = strsplit(sub.list.char, '\n')[[1]]
+    studstaff.list = c(studstaff.list, sub.list)
+  }
   
-  # faculty 人數
-  faculty.elem = remDr$findElement(using = 'class name', value = "faculty")
-  faculty = faculty.elem$getElementText()
+  # univ_name
+  uniinfo.elem = remDr$findElement(using = 'class name', value = "text-white")
+  univ_name = as.character( uniinfo.elem$getElementText() )
   
-  str_faculty = str_split(faculty, "\n")
-  
-  # 基本資訊
-  uniinfo.elem = remDr$findElement(using = 'class name', value = "uni_info")
-  uniinfo = uniinfo.elem$getElementText()
-  
-  str_uniinfo = str_split(uniinfo, "\n")
-  
-  # rank
-  rank = remDr$findElement(using = 'class name', value = "cont")
-  })
-  
-  # 有校名是基本的
-  nam = remDr$findElement(using = 'class name', value = "programeTitle")
-
   tryCatch({
-  qs_ranking = data.frame(n = nam$getElementText()[[1]],
-                          a = str_student[[1]][which(str_student[[1]] == "Total students")+1],
-                          b = str_student[[1]][which(str_student[[1]] == "PG students")[1]+1],
-                          c = str_student[[1]][which(str_student[[1]] == "UG students")[1]+1],
-                          d = str_student[[1]][which(str_student[[1]] == "International students")+1],
-                          e = str_student[[1]][which(str_student[[1]] == "PG students")[2]+1],
-                          f = str_student[[1]][which(str_student[[1]] == "UG students")[2]+1],
-                          g = str_faculty[[1]][which(str_faculty[[1]] == "Total faculty staff")+1],
-                          h = str_faculty[[1]][which(str_faculty[[1]] == "Int'l staff")+1],
-                          i = str_faculty[[1]][which(str_faculty[[1]] == "Domestic staff")+1],
-                          j = str_uniinfo[[1]][which(str_uniinfo[[1]] == "Status")+1],
-                          k = str_uniinfo[[1]][which(str_uniinfo[[1]] == "Research Output")+1],
-                          l = str_uniinfo[[1]][which(str_uniinfo[[1]] == "Size")+1],
-                          m = rank$getElementText()[[1]]
+  qs_ranking = data.frame(n = univ_name,
+                          a = studstaff.list[which(studstaff.list == "Total students")+1],
+                          b = studstaff.list[which(studstaff.list == "UG students")[1]+1],
+                          c = studstaff.list[which(studstaff.list == "PG students")[1]+1],
+                          d = studstaff.list[which(studstaff.list == "International students")+1],
+                          e = studstaff.list[which(studstaff.list == "UG students")[2]+1],
+                          f = studstaff.list[which(studstaff.list == "PG students")[2]+1],
+                          g = studstaff.list[which(studstaff.list == "Total faculty staff")+1],
+                          h = studstaff.list[which(studstaff.list == "Domestic staff")+1],
+                          i = studstaff.list[which(studstaff.list == "Int'l staff")+1]
                           )
   }, error = function(e){
-    qs_ranking = data.frame(n = nam$getElementText()[[1]],
+    qs_ranking = data.frame(n = univ_name,
                             a = NA,
                             b = NA,
                             c = NA,
@@ -108,20 +108,20 @@ for(i in 1:all_univ_n){
                             f = NA,
                             g = NA,
                             h = NA,
-                            i = NA,
-                            j = NA,
-                            k = NA,
-                            l = NA,
-                            m = NA)
+                            i = NA)
   })
     
   value = c("University", "Total students", "PG students",
             "UG students", "International students",
             "PG students", "UG students",
             "Total faculty staff", "Int'l staff",
-            "Domestic staff", "Status",
-            "Research Output", "Size", "rank")
+            "Domestic staff")
   colnames(qs_ranking) = value
   qs_ranking.table = rbind(qs_ranking.table, qs_ranking)
   
 }
+
+
+
+
+
